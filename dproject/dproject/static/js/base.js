14acddc47e294d1976d5023762544f063cb90aae
Miguel Galvão <miguelepgalvao@gmail.com>
2022-04-28 09:48:45 +0000

    added linebreaks to text areas and removed unnecessary folder

--- dproject/dproject/static/js/base.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', nav)
-
-function nav() {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-base');
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('show')
-    })
-}
